1f73d085779f973999adf88592b880beb0287dd6
bravomikekilo <bmk1221@126.com>
2018-04-19 23:52:28 +0800

    fix panel position problem

--- static/js/PatientPage.vue
+++ static/js/PatientPage.vue
@@ -1,23 +1,26 @@
 <template>
     <el-tabs v-model="aspect" tab-position="left">
-        <el-table :stripe="true" :border="true" :data="reservations">
-            <el-table-column
-                prop="id"
-                label="医生代码"
-            >
-            </el-table-column>
-            <el-table-column
-                prop='name'
-                label="医生姓名"
-            >
-            </el-table-column>
-            <el-table-column
-                prop="section"
-                label="预约时间"
-            >
-            </el-table-column>
-        </el-table>
+
         <el-tab-pane label="挂号" name='reserve'>
+            <p>我的预约</p>
+            <el-table :stripe="true" :border="true" :data="reservations">
+                <el-table-column
+                    prop="id"
+                    label="医生代码"
+                >
+                </el-table-column>
+                <el-table-column
+                    prop='name'
+                    label="医生姓名"
+                >
+                </el-table-column>
+                <el-table-column
+                    prop="section"
+                    label="预约时间"
+                >
+                </el-table-column>
+            </el-table>
+            <p>新建预约</p>
             <el-tabs @tab-click='fetchOffice' type='border-card'>
                 <el-tab-pane v-for="office in offices" v-bind:key='office'
                     v-bind:label="office" v-bind:name="office">
@@ -40,8 +43,6 @@
                     </el-table>
                 </el-tab-pane>
             </el-tabs>
-
-
         </el-tab-pane>
         <el-tab-pane label="病历查询" name='recordSearch'>
             病历查询
